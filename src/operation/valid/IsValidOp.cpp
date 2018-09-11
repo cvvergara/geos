5f766b17480a7bf1ed1c9d9b07a24dadb4d6f996
cvvergara <vicky@georepublic.de>
2018-09-11 11:14:16 -0500

    Using constants.h and stop using platform.h (closes #925)

--- src/operation/valid/IsValidOp.cpp
+++ src/operation/valid/IsValidOp.cpp
@@ -21,7 +21,7 @@
 
 #include "IndexedNestedRingTester.h" // TODO: private header>? --mloskot
 
-#include <geos/platform.h>
+#include <geos/constants.h>
 #include <geos/operation/valid/IsValidOp.h>
 #include <geos/operation/valid/ConsistentAreaTester.h>
 #include <geos/operation/valid/ConnectedInteriorTester.h>
@@ -93,8 +93,8 @@ IsValidOp::isValid()
 bool
 IsValidOp::isValid(const Coordinate &coord)
 {
-	if (! FINITE(coord.x) ) return false;
-	if (! FINITE(coord.y) ) return false;
+	if (! std::isfinite(coord.x) ) return false;
+	if (! std::isfinite(coord.y) ) return false;
 	return true;
 }
 
